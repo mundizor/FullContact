601f08bbcec0d15a337b5f1a83d0787874b14c92
mundizor <mattias_johansson996@hotmail.com>
2017-02-13 01:40:10 +0100

    first commit

---
+++ MyFullContactLibrary/FullContactPerson.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace MyFullContactLibrary
+{
+
+    public interface IFullContactApi
+    {
+        Task<FullContactPerson> LookupPersonByEmailAsync(string email);
+    }
+
+    public class FullContactPerson
+    {
+        public string likelihood;
+        public List<String> socialProfiles;
+        public List<String> contactInfo;
+
+        public FullContactPerson(string inLikeliood, List<String> inSocialProfiles, List<String> inContactInfo)
+        {
+            likelihood = inLikeliood;
+            socialProfiles = inSocialProfiles;
+            contactInfo = inContactInfo;
+        }
+
+    }
+
+    public class FullContactAPIClass : IFullContactApi
+    {
+
+        public Task<FullContactPerson> LookupPersonByEmailAsync(string email)
+        {
+            return LookupPerson(email);
+        }
+
+        private IEnumerable<JToken> AllChildren(JToken json)
+        {
+            foreach (var c in json.Children())
+            {
+                yield return c;
+                foreach (var cc in AllChildren(c))
+                {
+                    yield return cc;
+                }
+            }
+        }
+
+        private string getSingleValueFromJson(JObject jObj, string name)
+        {
+            return (string)jObj.Descendants()
+                .OfType<JProperty>()
+                .Where(p => p.Name == name)
+                .First()
+                .Value;
+        }
+
+        private List<JProperty> getMultipleValues(JObject jObj, string name)
+        {
+            var resultObjects = AllChildren(jObj)
+                .First(c => c.Type == JTokenType.Array && c.Path.Contains(name))
+                .Children<JObject>();
+
+            List<JProperty> tempList = new List<JProperty>();
+            foreach (JObject result in resultObjects)
+            {
+                foreach (JProperty property in result.Properties())
+                {
+
+                    tempList.Add(property);
+
+                }
+
+            }
+
+            return tempList;
+        }
+
+        private async Task<FullContactPerson> LookupPerson(string email)
+        {
+
+            string requestUrl = "https://api.fullcontact.com/v2/person.json?email=" + email + "&apiKey=xxxxxxxxxxxxxxxx";
+
+
+            var json = "";
+            try
+            {
+                json = new WebClient().DownloadString(requestUrl);
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("WebClient().DownloadString(requestUrl) failed. ");
+                Console.WriteLine("could not even get a response to parse to get the error code ");
+                Console.WriteLine("response: " + json);
+                Console.ReadKey();
+
+                return null;
+            }
+
+
+            JObject jObj = JObject.Parse(json);
+
+            //get status to see if we got a correct response.
+            string status = getSingleValueFromJson(jObj, "status");
+
+            //wrong return value. try again. 
+            if (status != "200")
+            {
+                Console.WriteLine("error code: " + status);
+                Console.WriteLine("try again");
+                Console.ReadKey();
+
+                return null;
+            }
+
+            //get likelihood from Jobject
+            string LH = getSingleValueFromJson(jObj, "likelihood");
+
+            List<String> SocialProfilesList = new List<String>();
+
+            //add every social profiles type to the list
+            foreach (JProperty property in getMultipleValues(jObj, "socialProfiles"))
+            {
+                if (property.Name != "type")
+                    continue;
+
+                SocialProfilesList.Add(property.ToString());
+
+            }
+
+            List<String> ContactInfoList = new List<String>();
+
+            //add all contact info to the list.
+            foreach (JProperty property in getMultipleValues(jObj, "contactInfo"))
+            {
+
+                ContactInfoList.Add(property.ToString());
+
+            }
+
+            return new FullContactPerson(LH, SocialProfilesList, ContactInfoList);
+        }
+
+    }
+
+
+}
+
---
+++ MyFullContactLibrary/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFullContactLibrary;
+using Nito.AsyncEx;
+
+namespace ConsoleAppMailEnter
+{
+    class Program
+    {
+
+        static int Main(string[] args)
+        {
+            try
+            {
+                return AsyncContext.Run(() => MainAsync(args));
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                return -1;
+            }
+        }
+
+        static async Task<int> MainAsync(string[] args)
+        {
+            bool ProgramRunning = true;
+            FullContactAPIClass yourClass = new FullContactAPIClass();
+
+            while (ProgramRunning) {
+
+                Console.WriteLine("skriv in en mail.");
+
+                string input = Console.ReadLine();
+
+                FullContactPerson FP = await yourClass.LookupPersonByEmailAsync(input);
+
+                if(FP != null)
+                    printInfo(FP);
+            }
+
+            return 1;
+        }
+
+        //prints all the info collected from the API
+        static void printInfo(FullContactPerson P)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("likelihood:");
+            Console.WriteLine(P.likelihood);
+            Console.WriteLine("");
+
+            Console.WriteLine("Contact Info:");
+
+            foreach (string infoString in P.contactInfo)
+                Console.WriteLine(infoString);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Social Media:");
+
+            foreach (string infoString in P.socialProfiles)
+                Console.WriteLine(infoString);
+
+            Console.WriteLine("");
+
+        }
+    }
+}
